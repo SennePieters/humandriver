2bd305c69377545bf6c3f184a5fcc443120e13ae
SennePieters <senne.pieters02@gmail.com>
2026-01-13 02:15:00 +0100

    deleted file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="humandriver",
-    version="1.0.2",
+    version="1.0.0",
     description="Human-like mouse and keyboard automation for zendriver",
     long_description=long_description,
     long_description_content_type="text/markdown",
